905b2e76c2abd09c05572dee012a1e58e5211ba8
Kenneth Conroy <kenneth.conroy@captivea.com>
2020-02-12 17:50:27 -0500

    Update models.py

--- product_history_tracking/models/models.py
+++ product_history_tracking/models/models.py
@@ -4,7 +4,7 @@ from odoo import api, fields, models, tools, _
 from odoo.exceptions import UserError
 from odoo.tools import float_is_zero, pycompat
 from odoo.addons import decimal_precision as dp
-
+from datetime import datetime
 import logging
 
 _logger = logging.getLogger('product')
