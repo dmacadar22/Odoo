084e9b56dba294478e76cbe25c22b319b1894e16
NathanQ <nathan.qjm@gmail.com>
2020-11-25 05:22:56 -0500

    Add module to override Company Phone Number on POS receipts

---
+++ pos_receipt_changes/__manifest__.py
@@ -0,0 +1,33 @@
+# Copyright 2018 Dinar Gabbasov <https://it-projects.info/team/GabbasovDinar>
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
+{
+    "name": """Adjusted POS Receipt""",
+    "summary": """Remove stuff from the default POS receipts""",
+    "category": "Point of Sale",
+    "images": [],
+    "version": "12.0",
+    "application": False,
+
+    "author": "NathanQj",
+    "website": "captivea.us",
+    "depends": [
+        "point_of_sale",
+    ],
+    "external_dependencies": {"python": [], "bin": []},
+    "data": [
+        "security/ir.model.access.csv",
+    ],
+    "demo": [
+    ],
+    "qweb": [
+        "static/src/xml/pos.xml",
+    ],
+
+    "post_load": None,
+    "pre_init_hook": None,
+    "post_init_hook": None,
+    "uninstall_hook": None,
+
+    "auto_install": False,
+    "installable": True,
+}
